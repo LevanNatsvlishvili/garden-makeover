73d3bf6a6cc0398e530ce89bec1bbefdfcd9a703
Levan Natsvlishvili <L.nats51@yahoo.com>
2026-03-19 18:08:29 +0400

    pushing for later

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
+import * as THREE from 'three';
 import { camera, renderer, scene } from './utils/renderer';
 import { controls } from './utils/controls/controls';
 import windowResizer from './utils/windowResizer';
-import { setupScene, loadPlacementTools, characterModel } from './scene/scene';
+import { setupScene, loadPlacementTools, characterModel, houseModel } from './scene/scene';
 import { updateCharacter } from './utils/controls/characterController';
 import { config } from './config/config';
+import { assetConfig } from './config/assetConfig';
 import './styles/style.css';
+
+const raycaster = new THREE.Raycaster();
+const screenPos = new THREE.Vector2();
+const charNDC = new THREE.Vector3();
+
+function isCharBehindSprite(sprite, charModel) {
+  charNDC.copy(charModel.position).project(camera);
+  screenPos.set(charNDC.x, charNDC.y);
+
+  raycaster.setFromCamera(screenPos, camera);
+  const hits = raycaster.intersectObject(sprite);
+
+  if (hits.length === 0) return false;
+
+  const charDist = camera.position.distanceTo(charModel.position);
+  return hits[0].distance < charDist;
+}
 
 async function init() {
   scene.add(camera);
@@ -34,6 +53,15 @@ async function init() {
 
     if (characterModel) {
       updateCharacter(characterModel, deltaSec);
+
+      if (houseModel) {
+        const houseRightEdge = houseModel.position.x + houseModel.scale.x * 0.5;
+        const charBehindX = characterModel.position.x < houseRightEdge;
+        const behind = charBehindX && isCharBehindSprite(houseModel, characterModel);
+        const target = behind ? 0.3 : 1.0;
+        const mat = houseModel.material;
+        mat.opacity += (target - mat.opacity) * 0.1;
+      }
     }
 
     controls.update();
